8a1cbebcedc33d85728e156bc33d89e0b79d77f3
phapdev <luongphap1810@gmail.com>
2025-11-02 06:50:37 +0700

    Add @vercel/analytics dependency and integrate Analytics component in index.tsx; update package.json and pnpm-lock.yaml accordingly.

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ErrorBoundary } from "../components/ErrorBoundary";
+import { Analytics } from "@vercel/analytics/next"
 import "./index.css";
 
 const rootElement = document.getElementById("root");
@@ -13,6 +14,7 @@ const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <ErrorBoundary>
+      <Analytics />
       <App />
     </ErrorBoundary>
   </React.StrictMode>,
